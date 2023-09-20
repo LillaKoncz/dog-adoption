55e835a291d50bc9b9b952cfe19a0b2fba5cc440
LillaKoncz <koncz.lilla92@gmail.com>
2023-09-20 19:55:14 +0200

    deleting about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import {Home} from './components/home/Home'
-import {About} from './components/about/About'
 import {Contact} from './components/contact/Contact'
 import {Adopt} from './components/dogs/Adopt'
 import { Dogdetails } from './components/dogs/Dogdetails'
@@ -16,7 +15,6 @@ function App() {
       <Nav/>
       <Routes>
         <Route path='/dog-adoption/' element={<Adopt/>}/>
-        <Route path='/dog-adoption/about' element={<About/>} />
         <Route path='/dog-adoption/contact' element={<Contact/>}/>
         <Route path='/dog-adoption/dogdetails/:id' element={<Dogdetails/>}/>
       </Routes>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -9,7 +9,6 @@ export const Nav = () => {
     <nav>
      <ul className='link-list my-4 mx-5'>
         <li className='links'><Link className='link' to='/dog-adoption/'>Home</Link></li>
-        <li className='links'><Link className='link' to='/dog-adoption/about'>About</Link></li>
         <li className='links'><Link className='link' to='/dog-adoption/contact'>Contact</Link></li>
      </ul>
     </nav>
